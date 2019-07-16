5d48da12b97f69b6a981d0a9b331249e399d5d6c
petermwash <petermwash48@gmail.com>
2019-07-16 14:07:28 +0300

    - Adding the project

---
+++ app/src/main/java/com/pemwa/topnews/NewsActivity.kt
@@ -0,0 +1,12 @@
+package com.pemwa.topnews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
